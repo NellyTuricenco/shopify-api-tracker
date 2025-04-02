b5a7218f7a81163f10db6f3516e7d6fd9e87b853
Nelea Turicenco <nelly@askphill.com>
2025-04-02 17:17:38 +0300

    update next.config to fix url to scripts & stylesheet during deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/Shopify-updates',
+  assetPrefix: '/Shopify-updates',
   trailingSlash: true,
 };
 
